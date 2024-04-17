a0c2eca76ccd66e400c6e257db07850760288af0
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-04-17 23:43:02 +0500

    udpate

--- supar_admin/loan_vewimore.php
+++ supar_admin/loan_vewimore.php
@@ -50,7 +50,14 @@ if (isset($_GET['loan_vewimore'])) {
             <!-- Main Content Header Card (End) -->
             <!-- Teacher Profile (Start) -->
             <div class="card box_mad_vewi px-4">
-                <h5 class="pb-2 border-bottom my-4 fs-7">تفصیلات</h5>
+                <div class="row border-bottom jameel-kasheeda">
+                    <div class="col-lg-6 ">
+                        <h5 class="pb-2 my-4 fs-7">تفصیلات</h5>
+                    </div>
+                    <div class="col-lg-6 text-end">
+                        <button onclick="printContent()" class="btn btn-primary pb-1 my-4 fs-3 "> پرنٹ</button>
+                    </div>
+                </div>
                 <div class="row">
                     <div class="col-lg-6">
                         <div class="info-container">
@@ -114,7 +121,7 @@ if (isset($_GET['loan_vewimore'])) {
                                     <span class="jameel-regular me-2">اُدھار لینے کا مقصد:</span>
                                     <span class="fs-6"><?= $fetch['purpose'] ?></span>
                                 </li>
-                               
+
                                 <li class="mb-4">
                                     <span class="jameel-regular me-2">تاریخ :</span>
                                     <span class="inter fs-6"><?= $fetch['loan_date'] ?></span>
@@ -143,6 +150,117 @@ if (isset($_GET['loan_vewimore'])) {
                 </div>
             </div>
             <!-- Teacher Profile  (End) -->
+            <!-- ==========print============= -->
+            <div class="content-wrapper " hidden id="printable-content">
+                <style>
+                    .logo img {
+                        width: 200px;
+                        height: auto;
+                    }
+
+                    .main {
+                        display: flex;
+                        align-items: center;
+                        margin-bottom: 20px;
+                        font-family: Jameel-Kasheeda;
+                    }
+
+                    .logo {
+                        margin-right: 20px;
+                    }
+
+                    .box {
+                        text-align: center;
+                        margin: 0 auto;
+                        font-family: Jameel-Kasheeda;
+                    }
+
+                    h1,
+                    h4 {
+                        margin: 5px 0;
+                        font-family: Jameel-Kasheeda;
+                    }
+
+                    table {
+                        width: 100%;
+                        border-collapse: collapse;
+                        border: 1px solid #000;
+                        font-family: Jameel-Kasheeda;
+                    }
+
+                    th,
+                    td {
+                        border: 1px solid #000;
+                        padding: 10px;
+                        text-align: left;
+                        font-family: Jameel-Kasheeda;
+                    }
+
+                    th {
+                        background-color: #f2f2f2;
+                        font-family: Jameel-Kasheeda;
+                    }
+
+                    td {
+                        background-color: #ffffff;
+                        font-family: Jameel-Kasheeda;
+                    }
+                </style>
+
+
+                <div class="main ">
+                    <div class="logo">
+                        <img src="../assets/images/hussiania.png" alt="">
+                    </div>
+                    <div class="box">
+                        <h3>آمدنی</h3>
+                        <h1>
+                            <?php
+                            $income_madarsa = explode(',', $fetch['madarasa_id']);
+                            foreach ($income_madarsa as $incomes_madarsa) {
+                                $seq_query = mysqli_query($conn, "SELECT * FROM `madarsa` WHERE `madarsa_id` ='$incomes_madarsa'");
+                                $sec = mysqli_fetch_object($seq_query);
+                                if ($sec) {
+                                    echo $sec->madarsa_name;
+                                }
+                            }
+                            ?>
+                        </h1>
+                        <h4>رسید نمبر :<?= $fetch['registration_number'] ?></h4>
+                    </div>
+
+                </div>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>وصول کنندہ</th>
+                            <th>فائدہ کی شرح</th>
+                            <th>ادھار کی مدت </th>
+                            <th>ادائیگی کا طریقہ</th>
+                            <th>اُدھار لینے کا مقصد</th>
+                            <th>اُدھار کا معاہدہ</th>
+                            <th>تاریخ </th>
+                            <th>مہینہ</th>
+                            <th>رقم</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?= $fetch['recipient_name'] ?></td>
+                            <td><?= $fetch['interest_rate'] ?></td>
+                            <td><?= $fetch['loan_duration'] ?></td>
+                            <td><?= $fetch['payment_method'] ?></td>
+                            <td><?= $fetch['purpose'] ?></td>
+                            <td><?= $fetch['agreement'] ?></td>
+                            <td><?= $fetch['loan_date'] ?></td>
+                            <td><?= $fetch['loan_month'] ?></td>
+                            <td><?= $fetch['loan_amount'] ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- ===========print and ============= -->
     <?php
     }
 } else {
@@ -155,6 +273,20 @@ if (isset($_GET['loan_vewimore'])) {
         </div>
         <div class="dark-transparent sidebartoggler"></div>
         </div>
+        <script>
+            function printContent() {
+                var printWindow = window.open('', '', 'height=400,width=600');
+                printWindow.document.write('<html dir="rtl"><head><title>مدرسہ حسینیہ</title>');
+                // Include Bootstrap CSS
+                printWindow.document.write('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">');
+                printWindow.document.write('</head><body>');
+                printWindow.document.write('<style>@font-face { font-family: "Jameel-Kasheeda"; src: url("../assets/font/urdu/Jameel-Noori-Nastaleeq-Kasheeda.ttf"); }</style>');
+                printWindow.document.write(document.getElementById('printable-content').innerHTML);
+                printWindow.document.write('</body></html>');
+                printWindow.document.close();
+                printWindow.print();
+            }
+        </script>
         <?php
         include "inc/mobileNavbar.php";
         include "inc/footer.php";
